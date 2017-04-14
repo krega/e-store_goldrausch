2f531baa1cabf108f6703cda9a1a842c1de77c8f
krega <kamil.regoc@gmail.com>
2017-04-14 12:26:13 +0200

    refactoring

---
+++ out/artifacts/main_war_exploded/WEB-INF/resources/js/controller.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Le on 1/11/2016.
+ */
+
+var cartApp = angular.module ("cartApp", []);
+
+cartApp.controller("cartCtrl", function ($scope, $http){
+
+    $scope.refreshCart = function (cartId) {
+        $http.get('/goldrausch/rest/cart/'+$scope.cartId).success(function (data) {
+            $scope.cart=data;
+        });
+    };
+
+    $scope.clearCart = function () {
+        $http.delete('/goldrausch/rest/cart/'+$scope.cartId).success($scope.refreshCart($scope.cartId));
+    };
+
+    $scope.initCartId = function (cartId) {
+        $scope.cartId = cartId;
+        $scope.refreshCart(cartId);
+    };
+
+    $scope.addToCart = function (productId) {
+        $http.put('/goldrausch/rest/cart/add/'+productId).success(function (data) {
+            $scope.refreshCart($http.get('/goldrausch/rest/cart/cartId'));
+            alert("Product successfully added to the cart!")
+        });
+    };
+
+    $scope.removeFromCart = function (productId) {
+        $http.put('/goldrausch/rest/cart/remove/'+productId).success(function (data) {
+            $scope.refreshCart($http.get('/goldrausch/rest/cart/cartId'));
+        });
+    };
+});
